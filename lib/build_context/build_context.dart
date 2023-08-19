5811e091ffc6e5cf54928004f463497af9c2bba3
birjuvachhani <brvachhani@gmail.com>
2023-08-19 12:07:02 +0530

    Add more BuildContext extensions

--- lib/build_context/build_context.dart
+++ lib/build_context/build_context.dart
@@ -12,6 +12,12 @@ extension ContextExtension on BuildContext {
   /// Short for `Theme.of(context)`.
   ThemeData get theme => Theme.of(this);
 
+  /// Short for `Theme.of(context).textTheme`.
+  TextTheme get textTheme => Theme.of(this).textTheme;
+
+  /// Short for `Theme.of(context).colorScheme`.
+  ColorScheme get colorScheme => Theme.of(this).colorScheme;
+
   /// Short for `MediaQuery.of(context)`.
   MediaQueryData get mediaQuery => MediaQuery.of(this);
 
@@ -20,6 +26,9 @@ extension ContextExtension on BuildContext {
 
   /// Short for `Navigator.of(context)`.
   NavigatorState get navigator => Navigator.of(this);
+
+  /// Short for `MediaQuery.sizeOf(context)`.
+  Size get size => MediaQuery.sizeOf(this);
 
   /// hides soft keyboard using platform channel
   void hideKeyboard() => helpers.hideKeyboard(this);
